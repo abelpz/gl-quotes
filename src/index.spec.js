d9fce709a31a2b82aa17c20f61c58cf9dc92a7be
Abel Pérez <abelper54@gmail.com>
2025-01-20 16:16:27 -0400

    added missing tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -7,7 +7,157 @@ const TEST_TIMOUT = 10000;
 const tests = [
   {
     params: {
-      name: "Testing deuteronomy highlighting error",
+      name: "",
+      bookId: "PSA",
+      ref: "6:8-9",
+      quote: "יְ֭הוָה & יְ֝הוָ֗ה",
+    },
+    expected: "Yahweh & Yahweh",
+  },
+  {
+    params: {
+      name: "Middle word not being highlighted",
+      bookId: "PSA",
+      ref: "6:8-9",
+      quote: "יְ֝הוָ֗ה & יְ֭הוָה & יְ֝הוָ֗ה",
+    },
+    expected: "Yahweh & Yahweh & Yahweh",
+  },
+  {
+    params: {
+      name: "Test 1",
+      bookId: "JUD",
+      ref: "1:10",
+      quote: "ὅσα & φυσικῶς ὡς τὰ ἄλογα ζῷα ἐπίστανται",
+      occurrence: 2,
+    },
+    expected: "what they understand by instinct as the unreasoning animals",
+    expectedSelections: [
+      {
+        text: "ὅσα",
+        occurrence: 2,
+      },
+      {
+        text: "φυσικῶς",
+        occurrence: 1,
+      },
+      {
+        text: "ὡς",
+        occurrence: 1,
+      },
+      {
+        text: "τὰ",
+        occurrence: 1,
+      },
+      {
+        text: "ἄλογα",
+        occurrence: 1,
+      },
+      {
+        text: "ζῷα",
+        occurrence: 1,
+      },
+      {
+        text: "ἐπίστανται",
+        occurrence: 1,
+      },
+    ],
+  },
+  {
+    params: {
+      name: "",
+      bookId: "3JN",
+      ref: "1:6-7",
+      quote:
+        "οὓς καλῶς ποιήσεις, προπέμψας ἀξίως τοῦ Θεοῦ; ὑπὲρ γὰρ τοῦ ὀνόματος ἐξῆλθον, μηδὲν λαμβάνοντες ἀπὸ τῶν ἐθνικῶν",
+      occurrence: 1,
+    },
+    only: true,
+    expected:
+      "whom you will do well to send on worthily of God & for they went out for the sake of the name, receiving nothing from the Gentiles",
+  },
+  {
+    params: {
+      name: "",
+      bookId: "2JN",
+      ref: "1:2",
+      quote: "καὶ μεθ’ ἡμῶν ἔσται",
+      occurrence: 1,
+    },
+    expected: "and will be with us",
+    expectedSelections: [
+      {
+        text: "καὶ",
+        occurrence: 1,
+      },
+      {
+        text: "μεθ’",
+        occurrence: 1,
+      },
+      {
+        text: "ἡμῶν",
+        occurrence: 1,
+      },
+      {
+        text: "ἔσται",
+        occurrence: 1,
+      },
+    ],
+  },
+  {
+    params: {
+      name: "",
+      bookId: "2JN",
+      ref: "1:2",
+      quote: "μεθ’ ἡμῶν",
+      occurrence: 1,
+    },
+    expected: "with us",
+    expectedSelections: [
+      {
+        text: "μεθ’",
+        occurrence: 1,
+      },
+      {
+        text: "ἡμῶν",
+        occurrence: 1,
+      },
+    ],
+  },
+  {
+    params: {
+      name: "",
+      bookId: "2JN",
+      ref: "1:2",
+      quote: "καὶ μεθ’",
+      occurrence: 1,
+    },
+    expected: "and & with",
+    expectedSelections: [
+      {
+        text: "καὶ",
+        occurrence: 1,
+      },
+      {
+        text: "μεθ’",
+        occurrence: 1,
+      },
+    ],
+  },
+  {
+    params: {
+      name: "",
+      bookId: "TIT",
+      ref: "1:4,9",
+      quote: "καὶ & καὶ",
+      occurrence: 3,
+    },
+    expected: "both & and",
+  },
+
+  {
+    params: {
+      name: "Consecutive quote parts separated by &",
       bookId: "DEU",
       ref: "1:5-6",
       quote:
@@ -15,6 +165,172 @@ const tests = [
     },
     expected:
       "Moses & explaining this law, saying & Yahweh our God spoke to us at Horeb, saying",
+  },
+  {
+    params: {
+      name: "",
+      bookId: "JOS",
+      ref: "21:27",
+      quote: "אֶת־גּוֹלָ֤ן & בְּעֶשְׁתְּרָ֖ה",
+    },
+    expected: "Golan & Be Eshterah",
+  },
+  {
+    params: {
+      name: "",
+      bookId: "GEN",
+      ref: "7:11",
+      quote: "בִּ⁠שְׁנַ֨ת שֵׁשׁ־מֵא֤וֹת שָׁנָה֙ לְ⁠חַיֵּי נֹ֔חַ",
+    },
+    expected: "In the six hundredth year of Noah’s life",
+  },
+  {
+    params: {
+      name: "",
+      bookId: "GEN",
+      ref: "1:21",
+      quote: "וְ⁠אֵ֣ת כָּל נֶ֣פֶשׁ הַֽ⁠חַיָּ֣ה הָֽ⁠רֹמֶ֡שֶׂת",
+    },
+    expected: "and every living creature that moves",
+  },
+  {
+    params: {
+      name: "",
+      bookId: "GEN",
+      ref: "1:3",
+      quote: "וַֽ⁠יְהִי אֽוֹר",
+    },
+    expected: "And there was light",
+  },
+  {
+    params: {
+      name: "",
+      bookId: "PSA",
+      ref: "3:2",
+      quote: "סֶֽלָה",
+    },
+    expected: "Selah",
+  },
+  {
+    params: {
+      name: "",
+      bookId: "JOS",
+      ref: "24:10",
+      quote: "וָ⁠אַצִּ֥ל אֶתְ⁠כֶ֖ם מִ⁠יָּדֽ⁠וֹ",
+    },
+    expected: "and I rescued you from his hand",
+  },
+  {
+    params: {
+      name: "",
+      bookId: "JOS",
+      ref: "1:11",
+      quote: "בְּ⁠ע֣וֹד׀ שְׁלֹ֣שֶׁת יָמִ֗ים",
+    },
+    expected: "within three days",
+  },
+  {
+    params: {
+      bookId: "JOS",
+      ref: "15:7",
+      quote:
+        "דְּבִרָ⁠ה֮ מֵ⁠עֵ֣מֶק עָכוֹר֒ & הַ⁠גִּלְגָּ֗ל & לְ⁠מַעֲלֵ֣ה אֲדֻמִּ֔ים & מֵי־עֵ֣ין שֶׁ֔מֶשׁ & עֵ֥ין רֹגֵֽל",
+    },
+    expected:
+      "to Debir from the Valley of Trouble & the Gilgal & of the ascent of Adummim & the waters of En Shemesh & En Rogel",
+  },
+  {
+    params: {
+      bookId: "JOS",
+      ref: "15:7",
+      quote:
+        "דְּבִרָ⁠ה֮ מֵ⁠עֵ֣מֶק עָכוֹר֒ & הַ⁠גִּלְגָּ֗ל & לְ⁠מַעֲלֵ֣ה אֲדֻמִּ֔ים & מֵי־עֵ֣ין שֶׁ֔מֶ & עֵ֥ין רֹגֵֽל",
+    },
+    expected: "",
+  },
+  {
+    params: {
+      bookId: "PHP",
+      ref: "1:1-20",
+      quote: "τινὲς μὲν καὶ & τὸν Χριστὸν κηρύσσουσιν",
+    },
+    expected: "Some indeed even proclaim Christ",
+  },
+  {
+    params: {
+      bookId: "GEN",
+      ref: "2:23",
+      quote: "עֶ֚צֶם מֵֽ⁠עֲצָמַ֔⁠י וּ⁠בָשָׂ֖ר מִ⁠בְּשָׂרִ֑⁠י",
+    },
+    expected: "is bone from my bones\nand flesh from my flesh",
+  },
+  {
+    params: {
+      bookId: "GEN",
+      ref: "4:23",
+      quote: "כִּ֣י אִ֤ישׁ הָרַ֨גְתִּי֙ לְ⁠פִצְעִ֔⁠י וְ⁠יֶ֖לֶד לְ⁠חַבֻּרָתִֽ⁠י",
+    },
+    expected:
+      "For I killed a man for my wound,\neven a young man for my bruise",
+  },
+  {
+    params: {
+      name: "",
+      bookId: "1PE",
+      ref: "1:24",
+      quote:
+        "πᾶσα σὰρξ ὡς χόρτος, καὶ πᾶσα δόξα αὐτῆς ὡς ἄνθος χόρτου. ἐξηράνθη ὁ χόρτος, καὶ τὸ ἄνθος ἐξέπεσεν,",
+      occurrence: 1,
+    },
+    expected: `All flesh is like grass,
+and all its glory is like the flower of the grass.
+The grass was dried up, and the flower fell off`,
+  },
+  {
+    params: {
+      name: "",
+      bookId: "1CO",
+      ref: "1:2",
+      quote: "τῇ ἐκκλησίᾳ τοῦ Θεοῦ & τῇ οὔσῃ ἐν Κορίνθῳ",
+      occurrence: 1,
+    },
+    expected: "to the church of God that is in Corinth",
+  },
+  {
+    params: {
+      bookId: "1PE",
+      ref: "1:7",
+      quote: "τὸ δοκίμιον ὑμῶν τῆς πίστεως & διὰ πυρὸς δὲ δοκιμαζομένου",
+      occurrence: 1,
+    },
+    expected: "the genuineness of your faith & but being tested by fire",
+  },
+  {
+    params: {
+      bookId: "1CO",
+      ref: "3:9",
+      quote: "Θεοῦ",
+      occurrence: -1,
+    },
+    expected: "God’s & God’s & God’s",
+  },
+  {
+    params: {
+      bookId: "1CO",
+      ref: "3:9",
+      quote: "συνεργοί & γεώργιον & οἰκοδομή",
+      occurrence: 1,
+    },
+    expected: "fellow workers & field & building",
+  },
+  {
+    params: {
+      bookId: "1CO",
+      ref: "7:32",
+      quote: "ὁ ἄγαμος & ἀρέσῃ",
+      occurrence: 1,
+    },
+    expected: "The unmarried man & he might please",
   },
 ];
 
